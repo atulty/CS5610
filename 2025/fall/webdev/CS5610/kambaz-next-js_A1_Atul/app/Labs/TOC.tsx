a05015bb251db237aaaffd98b3d506def7ddbc63
atulty <atul1820@gmail.com>
2025-09-17 22:38:40 -0400

    Add Assignment 1 project under kambaz-next-js_A1_Atul

---
+++ 2025/fall/webdev/CS5610/kambaz-next-js_A1_Atul/app/Labs/TOC.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import Lab1 from "./Lab1/page";
+
+export default function TOC() {
+  return (
+    <div>
+    <ul>
+      <li>
+        <Link href="/Labs" id="wd-labs-link">
+          Home </Link>
+      </li>
+      <li>
+        <Link href="/Labs/Lab1" id="wd-lab1-link">
+          Lab 1 </Link>
+      </li>
+      <li>
+        <Link href="/Labs/Lab2" id="wd-lab2-link">
+          Lab 2 </Link>
+      </li>
+      <li>
+        <Link href="/Labs/Lab3" id="wd-lab3-link">
+          Lab 3 </Link>
+      </li>
+      <li>
+        <Link href="/" id="wd-kambaz-link">
+          Kambaz </Link>
+      </li>
+      <li>
+        <Link href="https://github.com/piyush1420/kambaz-next-js" id="wd-github-link">
+          Github repo </Link>
+      </li>
+    </ul>
+    
+    
+
+    </div>
+  );
+}
