7392c188426d16f403d6e9f96bd8d11d3fda9e8a
atulty <atul1820@gmail.com>
2025-11-03 22:32:46 -0500

    Assignment 4: changes for build success

--- 2025/fall/webdev/CS5610/kambaz-next-js_A4_Atul/app/(kambaz)/Courses/[cid]/Assignments/ControlButton.tsx
+++ 2025/fall/webdev/CS5610/kambaz-next-js_A4_Atul/app/(kambaz)/Courses/[cid]/Assignments/ControlButton.tsx
@@ -1,43 +1,57 @@
 "use client";
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { FaPlus, FaTrash } from "react-icons/fa6";
-import { IoEllipsisVertical } from "react-icons/io5";
-import { FaPencil } from "react-icons/fa6";
-import { useSelector } from "react-redux";
 import { FaCheckCircle } from "react-icons/fa";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { FaPlus, FaTrash, FaPencil } from "react-icons/fa6";
+import { IoEllipsisVertical } from "react-icons/io5";
+import { useSelector } from "react-redux";
+
+type GroupControlButtonsProps = {
+  groupId?: string;
+  deleteGroup?: (groupId: string) => void;
+  editGroup?: (groupId: string) => void;
+};
 
 export default function GroupControlButtons({
   groupId,
-  deleteGroup,
-  editGroup,
-}: {
-  groupId: string;
-  deleteGroup: (groupId: string) => void;
-  editGroup: (groupId: string) => void;
-}) {
+  deleteGroup = () => console.warn("deleteGroup() not implemented"),
+  editGroup = () => console.warn("editGroup() not implemented"),
+}: GroupControlButtonsProps) {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const canEdit =
     currentUser?.role === "FACULTY" || currentUser?.role === "ADMIN";
+
+  const handleDelete = () => {
+    if (groupId) deleteGroup(groupId);
+    else console.warn("No groupId provided for deleteGroup");
+  };
+
+  const handleEdit = () => {
+    if (groupId) editGroup(groupId);
+    else console.warn("No groupId provided for editGroup");
+  };
 
   return (
     <div className="float-end d-flex align-items-center gap-2">
       {canEdit && (
         <>
           <FaPencil
-            onClick={() => editGroup(groupId)}
-            className="text-primary"
+            onClick={handleEdit}
+            className="text-primary cursor-pointer"
             title="Edit group"
           />
           <FaTrash
-            onClick={() => deleteGroup(groupId)}
-            className="text-danger"
+            onClick={handleDelete}
+            className="text-danger cursor-pointer"
             title="Delete group"
           />
         </>
       )}
-      <FaCheckCircle className="text-success" />
-      <FaPlus className="fs-4" />
-      <IoEllipsisVertical className="fs-4" />
+      <FaCheckCircle className="text-success" title="Completed" />
+      <FaPlus className="fs-4 cursor-pointer" title="Add" />
+      <IoEllipsisVertical
+        className="fs-4 cursor-pointer"
+        title="More options"
+      />
     </div>
   );
 }
